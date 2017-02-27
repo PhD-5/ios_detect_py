3092902e062699a4dd76548d7f555bfc2eb48d31
15cream <2256140303@qq.com>
2017-02-27 17:02:29 +0800

    1. 对执行流程进行了整理； 2. Nessus报告位置未处理 3. fuzz未处理 4. 数据库未处理 5. MITM 未检查

--- iOSAVD.py
+++ iOSAVD.py
@@ -9,31 +9,37 @@ import config
 import time
 
 
-class ios():
+class IOS():
     def __init__(self):
-
-        build_home_dir.build()
         data.client = set_ssl_conn(config.mobile_ip, config.ssh_port, config.mobile_user, config.mobile_password)
+        IOS.prepare_for_basic_info()
         # self.db = DBServer()
         # self.db.on()
+        self.t_static = static_analyzer()
+        self.app_dynamic_info = AppDynamicInfo(data.app_bundleID)
+        self.t_socket = SocketServerThread(self.app_dynamic_info)
+
+    @staticmethod
+    def prepare_for_basic_info():
+        build_home_dir.build()
         should_install.ask_for_user_choose()
         Utils.getInstalledAppList()
         Metadata().get_metadata()
         pre_clutch.clutch()
 
-    def detect(self):
-        # start java static analyse
+    def start_static_analyse(self):
         file_separator = os.path.sep
         os.chdir(os.path.abspath('.') + file_separator + 'lib')
-        t_static = static_analyzer()
-        t_static.start()
+        self.t_static.start()
         # need to change dir to root, because in static thread the dir is changed to lib dir.
         time.sleep(2)  # make sure java -jar in thread can get into directory lib
         os.chdir(os.path.abspath('..'))
 
-        # store the dynamic json results
-        app_dynamic_info = AppDynamicInfo(data.app_bundleID)
+    def finish_static_analyse(self):
+        self.t_static.join()
+        return True
 
+    def start_dynamic_check(self):
         # ask for should need detect MITM
         while True:
             user_input = raw_input('Do you want to detect MITM? [Y/N]')
@@ -42,7 +48,7 @@ class ios():
                 print '=   If you want to detect the MITM, please config on phone:     ='
                 print '=   OPEN the "MITM" and CLOSE the "Traffic"!                    ='
                 print '================================================================='
-                t_mitm_socket = SocketServerThread(app_dynamic_info)
+                t_mitm_socket = SocketServerThread(self.app_dynamic_info)
                 t_mitm_socket.start()
                 t_mitm_socket.join()
                 time.sleep(3)
@@ -54,72 +60,79 @@ class ios():
 
         # start local socket server to receive socket msg from iphone
         print 'Ready to start other detects, please CLOSE the MITM...'
-        t_socket = SocketServerThread(app_dynamic_info)
-        t_socket.start()
+        self.t_socket.start()
 
-        SharedLibrary().get()
-        protect_check().check()
-        String().get_strings()
+    def finish_dynamic_check(self):
+        self.t_socket.join()
+        return True
 
-        hosts = ','.join(String().get_url(data.strings))
-        # Nessus().scan(hosts, data.app_bundleID)1
-
-
-        # #########################end of dynamic detect####################
-        t_socket.join()
-
-
-        # copy the input data to class data
-        input_md5_list = get_md5(app_dynamic_info.user_input)
-        input_md5_list.extend(app_dynamic_info.user_input)
-        data.input_list = set(input_md5_list)
-
-
-        # detect sensitive content according to user input
-        input_json_parser = input_parser()
-        input_json_parser.parse_dynamic_info_for_input(app_dynamic_info)
-        data.dynamic_sensitive_json = app_dynamic_info.sensitive_json
-
-        # parse traffic json
-        traffic_parser = TrafficParser(app_dynamic_info.traffic_json_list)
-        traffic_parser.start_parser()
-        data.traffic_unsafe_result = traffic_parser.result
-
-        # parse MITM json
-        mitm_parser = MitmParser(app_dynamic_info.mitm_list)
-        mitm_parser.start_parse()
-        data.mitm_results = mitm_parser.results
-
-        # detect Hard Code
-        hardcode_detect = HarCodeDetect(app_dynamic_info.cccrtpy_json_list)
-        hardcode_detect.start_detect()
-        # print 'hardcode:',hardcode_detect.result
-
-        # detect sensitive data in files in sandbox
+    @staticmethod
+    def storage_check():
         Sql().get()
         Plist().get()
-
         # detect keychain
         keychain_checker = Keychain()
         keychain_checker.dump()
         data.keychain_values = keychain_checker.results
 
-        # start url fuzz (after dynamic, because need the urlsheme info got from dynamic detect)
+    @staticmethod
+    def binary_check():
+        SharedLibrary().get()
+        protect_check().check()
+        String().get_strings()
+
+    @staticmethod
+    def server_scan():
+        hosts = ','.join(String().get_url(data.strings))
+        # Nessus().scan(hosts, data.app_bundleID)1
+
+    def analyse(self):
+        # copy the input data to class data
+        input_md5_list = get_md5(self.app_dynamic_info.user_input)
+        input_md5_list.extend(self.app_dynamic_info.user_input)
+        data.input_list = set(input_md5_list)
+
+        # detect sensitive content according to user input
+        input_json_parser = input_parser()
+        input_json_parser.parse_dynamic_info_for_input(self.app_dynamic_info)
+        data.dynamic_sensitive_json = self.app_dynamic_info.sensitive_json
+
+        # parse traffic json
+        traffic_parser = TrafficParser(self.app_dynamic_info.traffic_json_list)
+        traffic_parser.start_parser()
+        data.traffic_unsafe_result = traffic_parser.result
+
+        # parse MITM json
+        mitm_parser = MitmParser(self.app_dynamic_info.mitm_list)
+        mitm_parser.start_parse()
+        data.mitm_results = mitm_parser.results
+
+        # detect Hard Code
+        hardcode_detect = HarCodeDetect(self.app_dynamic_info.cccrtpy_json_list)
+        hardcode_detect.start_detect()
+        # print 'hardcode:',hardcode_detect.result
+        # start url fuzz (after dynamic, because need the urlscheme info got from dynamic detect)
         # fuzzer = url_scheme_fuzzer(app_dynamic_info)
         # fuzzer.fuzz()
         # data.fuzz_result = fuzzer.results
 
-
-        # because static analyse cost long time, so join in the last
-        t_static.join()
-
-        # generate report
-        report_gen = Generator()
-        report_gen.generate()
+    def run(self):
+        self.start_static_analyse()
+        self.start_dynamic_check()
+        IOS.binary_check()
+        IOS.server_scan()
+        if self.finish_dynamic_check():
+            self.analyse()
+            IOS.storage_check()
+        if self.finish_static_analyse():
+            report_gen = Generator()
+            report_gen.generate()
+        self.clean()
 
     def clean(self):
         data.client.close()
-        self.db.down()
+        # self.db.down()
+
+IOS().run()
 
 
-ios().detect()
